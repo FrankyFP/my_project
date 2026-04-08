197886755891923faed529189e51f0f4bf6b565b
FrankyFP <raulitogamer360@gmail.com>
2026-04-08 15:56:50 +0200

    Creación controlador y entidad con migración

---
+++ src/Entity/Juegos.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JuegosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JuegosRepository::class)]
+class Juegos
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $Precio = null;
+
+    #[ORM\Column]
+    private ?int $Cantidad = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Plataforma = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrecio(): ?int
+    {
+        return $this->Precio;
+    }
+
+    public function setPrecio(int $Precio): static
+    {
+        $this->Precio = $Precio;
+
+        return $this;
+    }
+
+    public function getCantidad(): ?int
+    {
+        return $this->Cantidad;
+    }
+
+    public function setCantidad(int $Cantidad): static
+    {
+        $this->Cantidad = $Cantidad;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPlataforma(): ?string
+    {
+        return $this->Plataforma;
+    }
+
+    public function setPlataforma(string $Plataforma): static
+    {
+        $this->Plataforma = $Plataforma;
+
+        return $this;
+    }
+}
