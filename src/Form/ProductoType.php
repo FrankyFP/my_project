bf7ea75278507193eb9bafa4af3a5f8c24e179b4
FrankyFP <raulitogamer360@gmail.com>
2026-04-10 11:43:30 +0200

    Corrección final

--- src/Form/ProductoType.php
+++ src/Form/ProductoType.php
@@ -19,7 +19,7 @@ class ProductoType extends AbstractType
             ->add('descripcion')
             ->add('categoria', EntityType::class, [
                 'class' => Categoria::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nombre',
             ])
         ;
     }
